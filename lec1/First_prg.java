86fca6300d39435569d2a185dc302f0ef9398af6
Nidhi Vinod Nikam <nidhinikam1904@gmail.com>
2026-02-10 20:50:23 +0530

    Add First_prg Java program

---
+++ lec1/First_prg.java
@@ -0,0 +1,13 @@
+package lecture1;
+
+public class First_prg {
+	public static void main(String[] args) {
+		System.out.println("Hello, welcome!!");
+		
+	}
+
+}
+/*orange highlighted are keywords/reserved words
+class name, String , System are in uppercase. These all are class
+()these brackets symbolises methods/functions
+*/
